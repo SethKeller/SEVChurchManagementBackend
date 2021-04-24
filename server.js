2cb7ef430abd725616b95670fc6430dcf0128ec9
Austin Merritt <amerritt1999@gmail.com>
2021-04-24 17:48:27 -0500

    resyncing the database

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ const db = require("./models");
 
 db.sequelize.sync();
 
-// db.sequelize.sync({ force: true}).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
+db.sequelize.sync({ force: true}).then(() => {
+  console.log("Drop and re-sync db.");
+});
